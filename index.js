b6c15a50c54389df35c34c227aa73c13ce03520f
Yash-Singh1 <saiansh2525@gmail.com>
2021-01-09 14:01:22 -0800

    Implemented .toString() modification

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 'use strict';
-module.exports = (fn, name) => Object.defineProperty(fn, 'name', {value: name, configurable: true});
+module.exports = (fn, name) => {
+	Object.defineProperty(fn, 'name', {value: name, configurable: true});
+	const notArrowFuncReg = /^(?:async\s*)?(?:(\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*function/;
+	if (
+		!(
+			!notArrowFuncReg.test(fn.toString()) ||
+			fn.toString()
+				.replace(notArrowFuncReg, '')
+				.replace(/(\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))/g)
+				.trim()
+				.startsWith('(')
+		)
+	) {
+		const captured = /^((?:async\s*)?(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*function\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*)([^]*?)\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*\(/.exec(fn.toString());
+		const functionString = fn.toString();
+		fn.toString = (() => {
+			return functionString.slice(0, Math.max(0, captured[1].length)) +
+				name +
+				functionString.slice(Math.max(0, captured[1].length + captured[2].length));
+			/* eslint-disable-next-line no-extra-bind */
+		}).bind(fn);
+	}
+
+	return fn;
+};
--- test.js
+++ test.js
@@ -1,9 +1,78 @@
-import test from 'ava';
-import m from './';
+const test = require('ava');
+const m = require('./index.js');
 
-test(t => {
+test('name', t => {
 	function foo() {}
 	t.is(foo.name, 'foo');
 	m(foo, 'unicorn');
 	t.is(foo.name, 'unicorn');
+});
+
+test('toString', t => {
+	/* eslint-disable brace-style, capitalized-comments, func-name-matching, func-names, no-unused-vars, node/no-unsupported-features/es-syntax, object-shorthand, quotes, semi, space-before-blocks, space-before-function-paren, spaced-comment */
+	const fnStringCases = [
+		{
+			function: function blah (...args) {console.log("arguments:", args)},
+			name: 'hello',
+			string: 'function hello (...args) {console.log("arguments:", args)}'
+		},
+		{
+			function: async () => "fdas",
+			name: 'unicorn',
+			string: 'async () => "fdas"'
+		},
+		{
+			function: (hello, hi) => {},
+			name: 'dude',
+			string: '(hello, hi) => {}'
+		},
+		{
+			function: async async => {},
+			name: 'another',
+			string: 'async async => {}'
+		},
+		{
+			function: function () {},
+			name: 'name',
+			string: 'function () {}'
+		},
+		{
+			function: async /* function () {} */ function dude() {},
+			name: 'blue',
+			string: 'async /* function () {} */ function blue() {}'
+		},
+		{
+			function: function /*cmon*/ /*sdf*/ man() {},
+			name: 'man2',
+			string: 'function /*cmon*/ /*sdf*/ man2() {}'
+		},
+		{
+			function: function /* cmon */ /* sdf */ man() {},
+			name: 'man2',
+			string: 'function /* cmon */ /* sdf */ man2() {}'
+		},
+		{
+			function: function /*cmon*//*sdf*/ man() {},
+			name: 'man2',
+			string: 'function /*cmon*//*sdf*/ man2() {}'
+		},
+		{
+			function: async /*function () {}*/ function /*sdf*/sky/*hello*/() {},
+			name: 'green',
+			string: 'async /*function () {}*/ function /*sdf*/green/*hello*/() {}'
+		},
+		{
+			/* eslint-disable indent */
+			function: function // stuff
+another(){},
+			/* eslint-enable indent */
+			name: 'line',
+			string: 'function // stuff\nline(){}'
+		}
+	];
+	/* eslint-enable */
+	fnStringCases.forEach(element => {
+		element.function = m(element.function, element.name);
+		t.is(element.function.toString(), element.string);
+	});
 });
