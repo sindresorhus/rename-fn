0de16d0b02d51106f23487f23be3ec53117c125c
Yash-Singh1 <saiansh2525@gmail.com>
2021-01-10 15:21:29 -0800

    :)

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 'use strict';
 module.exports = (fn, name) => {
 	Object.defineProperty(fn, 'name', {value: name, configurable: true});
-	const notArrowFuncReg = /^(?:async\s*)?(?:(\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*function(?:\*)?/;
+	const functionString = fn.toString();
 	if (
 		!(
-			!notArrowFuncReg.test(fn.toString()) ||
-			fn.toString()
-				.replace(notArrowFuncReg, '')
+			!/^(?:async\s*)?(?:(\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*(?:function)?\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*\*?.*?\s*(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n)\s*)*\(/.test(functionString) ||
+			functionString
+				.replace(/(async|function|\s*|(\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)/g, '')
 				.replace(/(\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))/g)
 				.trim()
 				.startsWith('(')
-		)
+		) && functionString.indexOf('class') !== 0
 	) {
-		const captured = /^((?:async\s*)?(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*function(?:\*)?\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*)([^]*?)\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*\(/.exec(fn.toString());
-		const functionString = fn.toString();
+		const captured = /^((?:get|set)?(?:async\s*)?(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*(?:function)?\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*\*?\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*)([^]*?)\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*\(/.exec(functionString);
 		fn.toString = (() => {
-			return functionString.slice(0, Math.max(0, captured[1].length)) +
-				name +
-				functionString.slice(Math.max(0, captured[1].length + captured[2].length));
+			return functionString.slice(0, captured[1].length) + name + functionString.slice(captured[1].length + captured[2].length);
 			/* eslint-disable-next-line no-extra-bind */
 		}).bind(fn);
+	} else if (functionString.indexOf('class') === 0) {
+		const captured = /^(class\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*)([^]*?)\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*(?:extends|{)/.exec(functionString);
+		if (captured[2] !== '') {
+			fn.toString = (() => {
+				return functionString.slice(0, captured[1].length) + name + functionString.slice(captured[1].length + captured[2].length);
+				/* eslint-disable-next-line no-extra-bind */
+			}).bind(fn);
+		}
 	}
 
 	return fn;
--- test.js
+++ test.js
@@ -9,7 +9,7 @@ test('name', t => {
 });
 
 test('toString', t => {
-	/* eslint-disable brace-style, capitalized-comments, func-name-matching, func-names, no-unused-vars, node/no-unsupported-features/es-syntax, object-shorthand, quotes, semi, space-before-blocks, space-before-function-paren, spaced-comment */
+	/* eslint-disable brace-style, capitalized-comments, func-name-matching, func-names, generator-star-spacing, no-multi-spaces, no-new-func, no-unused-vars, no-useless-computed-key, node/no-unsupported-features/es-syntax, object-curly-spacing, object-shorthand, quotes, semi, space-before-blocks, space-before-function-paren, spaced-comment, space-in-parens */
 	const fnStringCases = [
 		{
 			function: function blah (...args) {console.log("arguments:", args)},
@@ -68,6 +68,101 @@ another(){},
 			/* eslint-enable indent */
 			name: 'line',
 			string: 'function // stuff\nline(){}'
+		},
+		{
+			function: function* generator() {},
+			name: 'gen',
+			string: 'function* gen() {}'
+		},
+		{
+			function: function * generator() {},
+			name: 'gen',
+			string: 'function * gen() {}'
+		},
+		{
+			function: function /* comments */* generator() {},
+			name: 'gen',
+			string: 'function /* comments */* gen() {}'
+		},
+		{
+			function: async function /* comments */ * generator() {},
+			name: 'gen',
+			string: 'async function /* comments */ * gen() {}'
+		},
+		{
+			function: async /*function () {}*/ function /* function* () {console.log("fakegen")} */ * /*sdf*/sky/*hello*/() {},
+			name: 'gen',
+			string: 'async /*function () {}*/ function /* function* () {console.log("fakegen")} */ * /*sdf*/gen/*hello*/() {}'
+		},
+		{
+			/* eslint-disable indent */
+			function: async /*function () {}*/ function /* function* () {console.log("fakegen")} */
+// hello
+* /*sdf*/sky/*hello*/() {},
+			/* eslint-enable indent */
+			name: 'gen',
+			string: 'async /*function () {}*/ function /* function* () {console.log("fakegen")} */\n// hello\n* /*sdf*/gen/*hello*/() {}'
+		},
+		{
+			function: class classy {},
+			name: 'B',
+			string: 'class B {}'
+		},
+		{
+			/* eslint-disable indent */
+			function: class sample extends (class another {}) { /* constructor */ constructor(stuff) { // constructed method
+// look down
+super();
+this.stuff = stuff;
+} },
+			/* eslint-enable indent */
+			name: 'extendedSample',
+			string: 'class extendedSample extends (class another {}) { /* constructor */ constructor(stuff) { // constructed method\n// look down\nsuper();\nthis.stuff = stuff;\n} }'
+		},
+		{
+			function: ({ [0](){} }[0]),
+			name: '[1]',
+			string: '[1](){}'
+		},
+		{
+			function: ({ a(){} }.a),
+			name: 'b',
+			string: 'b(){}'
+		},
+		{
+			function: ({ *a(){} }.a),
+			name: 'b',
+			string: '*b(){}'
+		},
+		{
+			function: ({ * /* I am totally defining a generator method */ a(){} }.a),
+			name: 'b',
+			string: '* /* I am totally defining a generator method */ b(){}'
+		},
+		{
+			function: (Object.getOwnPropertyDescriptor({ get /* I am totally defining a getter */ a ( ){ return 'nothing'  } }, "a").get),
+			name: 'b',
+			string: 'get /* I am totally defining a getter */ b ( ){ return \'nothing\'  }'
+		},
+		{
+			function: (Object.getOwnPropertyDescriptor({ get a(){return 'a'}, set /* I am totally defining a setter */ a ( x){} }, "a").set),
+			name: 'b',
+			string: 'set /* I am totally defining a setter */ b ( x){}'
+		},
+		{
+			function: (function(){}).bind(),
+			name: 'binded',
+			string: 'function () { [native code] }'
+		},
+		{
+			function: new Function("a", "return a"),
+			name: 'anonymousFunc',
+			string: 'function anonymousFunc(a\n) {\nreturn a\n}'
+		},
+		{
+			function: String,
+			name: 'myString',
+			string: 'function myString() { [native code] }'
 		}
 	];
 	/* eslint-enable */
