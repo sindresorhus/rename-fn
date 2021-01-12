34b85d3df6e3bb005cdcf57be345025b6f57b8c7
Yash-Singh1 <saiansh2525@gmail.com>
2021-01-11 18:44:00 -0800

    Remove binding to prevent bound log in REPL

--- index.js
+++ index.js
@@ -17,21 +17,18 @@ module.exports = (fn, name) => {
 		const captured = /^((?:get|set)?(?:async\s*)?(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*(?:function)?\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*\*?\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*)([^]*?)\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*\(/.exec(functionString);
 		fn.toString = (() => {
 			return functionString.slice(0, captured[1].length) + name + functionString.slice(captured[1].length + captured[2].length);
-			/* eslint-disable-next-line no-extra-bind */
-		}).bind(fn);
+		});
 	} else if (functionString.indexOf('class') === 0) {
 		const captured = /^(class\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*)([^]*?)\s*(?:(?:\/\*[^]*?\*\/|\/\/[^]*?(?:\r\n|\r|\n))\s*)*(?:extends|{)/.exec(functionString);
 		if (captured[2] !== '') {
 			fn.toString = (() => {
 				return functionString.slice(0, captured[1].length) + name + functionString.slice(captured[1].length + captured[2].length);
-				/* eslint-disable-next-line no-extra-bind */
-			}).bind(fn);
+			});
 		}
 	} else if (/^(get|set)/.test(functionString)) {
 		fn.toString = (() => {
 			return name + functionString.slice(fn.name.length);
-			/* eslint-disable-next-line no-extra-bind */
-		}).bind(fn);
+		});
 	}
 
 	return fn;
