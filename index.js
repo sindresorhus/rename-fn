5e1fba199c77f4a311a15a9b56dce52d54a182ec
Yash-Singh1 <saiansh2525@gmail.com>
2021-01-13 16:22:18 -0800

    Helper function, reg var, & shorter reg

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 /* eslint-disable func-names, no-extra-bind */
 
 'use strict';
+
+function nameOfMethod(methodString) {
+	return methodString.slice(/([^]*?)[\s/(]/.exec(methodString)[1].length);
+}
+
 module.exports = (fn, name) => {
 	// Set the name property first
 	Object.defineProperty(fn, 'name', {value: name, configurable: true});
 
 	// Cache the string to prevent a recursive function
 	const functionString = fn.toString();
+
+	const edgeCaseMethodNameReg = /^(get|set|async|function)/;
 
 	// Make sure that the function is not:
 	//	1. An arrow function (can't have a name)
@@ -36,7 +43,7 @@ module.exports = (fn, name) => {
 			//	3. Methods with their name as get*/set*
 			if (
 				functionString.indexOf('class') !== 0 &&
-				(!/^(get|set|async|function)/.test(functionString) || !functionString.slice(/([^]*?)[\s/(]/.exec(functionString)[1].length).replace(/\s/g, '').replace(/(\/\*[^]*?\*\/|\/\/[^]*?\n)\s*/g, '').trim().startsWith('('))
+				(!edgeCaseMethodNameReg.test(functionString) || !nameOfMethod(functionString).replace(/\s/g, '').replace(/\/\*[^]*?\*\/|\/\/[^]*?\n/g, '').trim().startsWith('('))
 			) {
 				const captured = /^((?:get|set)?(?:async\s*)?(?:\/(?:\*[^]*?\*\/|\/[^]*?\n)\s*)*(?:function)?\s*(?:\/(?:\*[^]*?\*\/|\/[^]*?\n)\s*)*\*?\s*(?:\/(?:\*[^]*?\*\/|\/[^]*?\n)\s*)*)([^]*?)\s*(?:\/(?:\*[^]*?\*\/|\/[^]*?\n)\s*)*\(/.exec(functionString);
 				return functionString.slice(0, captured[1].length) + name + functionString.slice(captured[1].length + captured[2].length);
@@ -47,8 +54,8 @@ module.exports = (fn, name) => {
 				return functionString.slice(0, captured[1].length) + name + functionString.slice(captured[1].length + captured[2].length);
 			}
 
-			if (/^(get|set|async|function)/.test(functionString)) {
-				return name + functionString.slice(/([^]*?)[\s/(]/.exec(functionString)[1].length);
+			if (edgeCaseMethodNameReg.test(functionString)) {
+				return name + nameOfMethod(functionString);
 			}
 		}).bind(fn);
 	}
