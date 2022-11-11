a53b06f6af0adaea48444261a4a23fd24a5e8982
Leon Si <leon@leondreamed.com>
2022-11-10 20:04:09 -0500

    refactor: tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,7 +1,7 @@
 import {expectType} from 'tsd';
 import renameFunction from './index.js';
 
-function f() {}
-const g = () => {}
-expectType<typeof f>(renameFunction(f, 'foo'));
-expectType<typeof g>(renameFunction(g, 'bar'));
+function foo() {}
+const bar = () => {}
+expectType<typeof foo>(renameFunction(foo, 'unicorn'));
+expectType<typeof bar>(renameFunction(bar, 'unicorn'));
